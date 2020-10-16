0aa110becad43d8ec7b18139a43841d4652b3c5b
kelly r maass <kmaass77627@gmail.com>
2020-10-16 21:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your Circle class here
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+  get diameter() {
+
+  }
+
+  get circumference() {
+
+  }
+
+  get area() {
+
+  }
+
+
+}
